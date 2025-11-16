a77733143cb6ad0c6d3a9577317979efc8273106
SkinnyAstro <sartapeprasad@gmail.com>
2025-11-16 21:09:25 +0530

    Array code added

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int [] marks = new int[3]; // here 3 is the size of the array or the memory allocation
+        marks[0]= 78;
+        marks[1]= 67;
+        marks[2]= 89;
+        //marks[4]=56;
+
+        for (int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+
+}
