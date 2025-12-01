2c73e10a5512fe19499bd47db72d63844a0ef4e2
Skinny Astro <sartapeprasad@gmail.com>
2025-12-01 17:53:34 +0530

    Array list code added

---
+++ ArrayList/ArrayLists.java
@@ -0,0 +1,12 @@
+package Practice.ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        //ArrayList<>list2 = new ArrayList<String>();
+
+    }
+}
